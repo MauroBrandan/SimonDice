9464b2489a351c846dc56c380af40c8ede07e8a6
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-01-13 00:53:19 -0300

    Generando una secuencia de números para los colores

--- index.js
+++ index.js
@@ -7,10 +7,22 @@ const btnEmpezar = document.getElementById('btnEmpezar')
 class Juego{
     constructor(){
         this.inicializar()
+        this.generarSecuencia()
     }
 
     inicializar(){
         btnEmpezar.classList.add('hide')
+        this.nivel = 1
+        this.colores = {
+            celeste,
+            violeta,
+            naranja,
+            verde
+        }
+    }
+
+    generarSecuencia(){
+        this.secuncia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
     }
 }
 
@@ -19,6 +31,6 @@ class Juego{
 btnEmpezar.addEventListener('click', empezarJuego)
 
 function empezarJuego() {
-    let juego = new Juego()
+    window.juego = new Juego()
 
 }
